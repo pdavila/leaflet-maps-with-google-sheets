33d232135007cfe0880dc874bef83a009a797ab6
pdavila <patricio.davila.ca@gmail.com>
2020-03-10 17:49:11 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1LK2tShRzSqU6T5EnqOXTd8WudzopURC9oiYZViyxJfI/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ktosTZjUu7Hw59BlbIQmeFLIdlinem-hyQK0Uc31QB8/edit#gid=0';
